023f07b5edf41e2bf7c933432076a4fe462c4484
luiz.fernando <luiz-fsilva37@hotmail.com>
2024-03-19 20:11:26 -0300

    Cabeçalho da aplicação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,23 @@
+import logo from '@/assets/logo.svg';
 import { globalStyles } from "@/styles/global";
+import { Container, Header } from '@/styles/pages/app';
 import type { AppProps } from "next/app";
+import Image from 'next/image';
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  console.log(logo);
+
+  return (
+    <Container>
+      <Header>
+        <Image
+          src={logo}
+          alt=''
+        />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  );
 }
