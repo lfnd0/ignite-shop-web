0c890d0774f40af9e5501ddbd43b46e6790d057f
luiz.fernando <luiz-fsilva37@hotmail.com>
2024-03-19 22:27:32 -0300

    Criando carrossel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,46 @@
-import { HomeContainer, Product } from "@/styles/pages/home";
-import Image from "next/image";
-
 import tshirt1 from '@/assets/items/1.png';
 import tshirt2 from '@/assets/items/2.png';
 import tshirt3 from '@/assets/items/3.png';
+import { HomeContainer, Product } from "@/styles/pages/home";
+import 'keen-slider/keen-slider.min.css';
+import { useKeenSlider } from 'keen-slider/react';
+import Image from "next/image";
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  });
+
   return (
-    <HomeContainer>
-      <Product>
-        <Image src={tshirt1} width={520} height={480} alt=""/>
+    <HomeContainer ref={sliderRef} className='keen-slider'>
+      <Product className="keen-slider__slide">
+        <Image src={tshirt1} width={520} height={480} alt="" />
         <footer>
           <strong>Camiseta P</strong>
           <span>R$ 79,99</span>
         </footer>
       </Product>
-      <Product>
-        <Image src={tshirt2} width={520} height={480} alt=""/>
+      <Product className="keen-slider__slide">
+        <Image src={tshirt2} width={520} height={480} alt="" />
         <footer>
           <strong>Camiseta M</strong>
           <span>R$ 79,99</span>
         </footer>
       </Product>
-      <Product>
-        <Image src={tshirt3} width={520} height={480} alt=""/>
+      <Product className="keen-slider__slide">
+        <Image src={tshirt3} width={520} height={480} alt="" />
         <footer>
           <strong>Camiseta G</strong>
+          <span>R$ 79,99</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={tshirt1} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta GG</strong>
           <span>R$ 79,99</span>
         </footer>
       </Product>
