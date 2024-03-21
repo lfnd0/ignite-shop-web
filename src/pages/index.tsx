5c3bccff816683d29ec4ee48c3c7c8f96fbc5dd1
luiz.fernando <luiz-fsilva37@hotmail.com>
2024-03-21 18:46:26 -0300

    Utilizando SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { stripe } from '@/lib/stripe';
 import { HomeContainer, Product } from "@/styles/pages/home";
 import 'keen-slider/keen-slider.min.css';
 import { useKeenSlider } from 'keen-slider/react';
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Image from "next/image";
 import Stripe from 'stripe';
 
@@ -40,7 +40,7 @@ export default function Home({ products }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price'],
   });
@@ -58,7 +58,8 @@ export const getServerSideProps: GetServerSideProps = async () => {
 
   return {
     props: {
-      products
+      products,
     },
+    revalidate: 60 * 60 * 2, // s * min * h
   };
 };
