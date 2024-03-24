a38a9651705de6b5344b2f693e267d969eeda67f
luiz.fernando <luiz-fsilva37@hotmail.com>
2024-03-23 23:58:41 -0300

    Estrutura da página de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,21 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product"
 import { useRouter } from "next/router"
 
 export default function Product() {
   const { query } = useRouter()
 
-  return(
-    <h1>Product: {JSON.stringify(query)}</h1>
+  return (
+    <ProductContainer>
+      <ImageContainer>
+      </ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta P</h1>
+        <span>R$ 79,99</span>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero earum repellendus incidunt dicta dolore, modi dolores minima facere ullam quasi, suscipit impedit? Ab delectus quae itaque dolores laborum officia eum!</p>
+        <button>
+          Comprar
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
