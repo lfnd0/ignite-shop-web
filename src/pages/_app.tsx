fb2673f29ddbaa080f56388ee900df2e4d5fd441
luiz.fernando <luiz-fsilva37@hotmail.com>
2024-03-28 23:56:33 -0300

    Navegação via Link (update)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { globalStyles } from '@/styles/global'
 import { Container, Header } from '@/styles/pages/app'
 import type { AppProps } from 'next/app'
 import Image from 'next/image'
+import Link from 'next/link'
 
 globalStyles()
 
@@ -10,7 +11,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <Image src={logo} priority={false} alt="" />
+        <Link href={'/'}>
+          <Image src={logo} priority={false} alt="" />
+        </Link>
       </Header>
       <Component {...pageProps} />
     </Container>
